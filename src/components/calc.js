fa1f64588548c3f18064f8507276ccdbcd055e3d
Rask467 <conner.jensen@hey.com>
2022-04-09 11:45:43 -0600

    Update style

--- src/components/calc.js
+++ src/components/calc.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import { joePerSec } from '../utils/farms'
 import { ethers } from 'ethers'
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+import joe from '../joe.png'
+import css from './calc.css'
 
 export default function Calc() {
     const [pairs, setPairs] = useState([])
@@ -11,7 +13,7 @@ export default function Calc() {
     const [selectedBoostedPool, setSelectedBoostedPool] = useState({})
     const [token0Value, setToken0Value] = useState(0)
     const [token1Value, setToken1Value] = useState(0)
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(true)
     const [poolShare, setPoolShare] = useState(0)
     const [veJoeOrJoe, setVeJoeOrJoe] = useState('veJoe')
     const [veJoeData, setVeJoeData] = useState({})
@@ -22,6 +24,8 @@ export default function Calc() {
     const [totalAllocPoint, setTotalAllocPoint] = useState(0)
     const [userAddr, setUserAddr] = useState('')
     const [userVeJoe, setUserVeJoe] = useState({})
+    const [currentBoostedAPR, setCurrentBoostedAPR] = useState(0)
+    const [estimatedBoostedAPR, setEstimatedBoostedAPR] = useState(0)
     const BOOSTEDURL = 'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/boosted-master-chef'
     const EXCHANGEURL = 'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/exchange'
     const VEJOEURL = 'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/vejoe'
@@ -188,33 +192,69 @@ export default function Calc() {
             console.log('totalAllocPoint: ', totalAllocPoint)
             console.log('joePerSec: ', joePerSecond)
             const poolsJoePerSec = (boostedPool.allocPoint/totalAllocPoint) * joePerSecond
-            console.log('poolsJoePerSec: ', poolsJoePerSec)
-            const baseJoeRewardsPerSec = poolsJoePerSec * 0.60
-            console.log('baseJoeRewardsPerSec: ', baseJoeRewardsPerSec)
             const joePriceInUSD = joePair.reserve1 / joePair.reserve0
+            const baseJoeRewardsPerSec = poolsJoePerSec * 0.50
+          
+            // Possible second way to calc apr
+            const userLiq = p.totalSupply
+            // const rewardsPerSec = (userLiq * poolsJoePerSec * 0.5) / p.totalSupply
+            // const rewardsPerYear = rewardsPerSec * SECONDSPERYEAR
+            // const apr = (((rewardsPerYear/(10**18)) * joePriceInUSD) / p.reserveUSD) * 100
+            // console.log("APR: ", apr)
+
+            console.log('userLiq: ', userLiq)
+            console.log('poolsJoePerSec: ', poolsJoePerSec)
+            console.log('baseJoeRewardsPerSec: ', baseJoeRewardsPerSec)
             console.log('joePriceInUSD: ', joePriceInUSD)
-            const baseJoeRewardsPerYear = baseJoeRewardsPerSec * SECONDSPERYEAR
-            console.log('baseJoeRewardsPerYear: ', baseJoeRewardsPerYear)
-            const baseJoeAPR = (((baseJoeRewardsPerYear/(10**18)) * joePriceInUSD) / p.reserveUSD) * 100
-            console.log('baseJoeAPR', baseJoeAPR)
             console.log('p.volumeUSD: ', p.volumeUSD)
             console.log('p.reserveUSD: ', p.reserveUSD)
-            let volumeUSD
-            if (p.volumeUSD !== '0') {
-                volumeUSD = p.volumeUSD
-            } else {
-                const volumeToken0USD = p.volumeToken0 * p.token0Price
-                const volumeToken1USD = p.volumeToken1 * p.token1Price
-                volumeUSD = volumeToken0USD + volumeToken1USD
-            }
-            const basePoolAPR = ((volumeUSD * 0.0025) / p.reserveUSD) * 100
-            console.log('basePoolAPR', basePoolAPR)
-            setBaseAPR(baseJoeAPR + basePoolAPR)
+            console.log('joeStake: ', joeStake)
+            
+            const baseAPR = calcBaseAPR(p, baseJoeRewardsPerSec, joePriceInUSD)
+            console.log('baseAPR', baseAPR)
+            setBaseAPR(baseAPR)
 
-            const boostedJoeRewardsPerSec = poolsJoePerSec * 0.40
-            const boostedJoeRewardsPerYear = boostedJoeRewardsPerSec * SECONDSPERYEAR
+            let currentVeJoeBal = 0
+            if (userVeJoe !== null) {
+                currentVeJoeBal = userVeJoe.veJoeBalance
+            }
+            // I need to use the real numbers for userLiq in these equations
+            setCurrentBoostedAPR(calcBoostedAPR(p, userLiq, currentVeJoeBal, joePriceInUSD))
+            setEstimatedBoostedAPR(calcBoostedAPR(p, userLiq, joeStake, joePriceInUSD))
         }
-    }, [selectedPairID, userAddr])
+    }, [selectedPairID, userAddr, joeStake, userVeJoe])
+
+    function calcBoostedAPR(p, userLiq, veJoe, joePriceInUSD) {
+        const userFarmFactor = Math.sqrt(userLiq * veJoe)
+        const totalFarmFactor = 1
+        console.log('userFarmFactor: ', userFarmFactor)
+        const userBoostedRewardsPerSec = (userFarmFactor * joePerSecond * 0.5) / totalFarmFactor
+        console.log('userBoostedRewardsPerSec: ', userBoostedRewardsPerSec)
+        const userBoostedRewardsPerYear = userBoostedRewardsPerSec * SECONDSPERYEAR
+        console.log('userBoostedRewardsPerYear: ', userBoostedRewardsPerYear)
+        const boostedAPR = ((userBoostedRewardsPerYear / (10**18) * joePriceInUSD) / p.reserveUSD) * 100
+        console.log('boostedAPR: ', boostedAPR)
+        return boostedAPR
+    }
+
+    function calcBaseAPR(p, baseJoeRewardsPerSec, joePriceInUSD) {
+        const baseJoeRewardsPerYear = baseJoeRewardsPerSec * SECONDSPERYEAR
+        const baseJoeAPR = (((baseJoeRewardsPerYear/(10**18)) * joePriceInUSD) / p.reserveUSD) * 100
+
+        let volumeUSD
+        if (p.volumeUSD !== '0') {
+            volumeUSD = p.volumeUSD
+        } else {
+            const volumeToken0USD = p.volumeToken0 * p.token0Price
+            const volumeToken1USD = p.volumeToken1 * p.token1Price
+            volumeUSD = volumeToken0USD + volumeToken1USD
+        }
+        const basePoolAPR = ((volumeUSD * 0.0025) / p.reserveUSD) * 100
+
+        console.log('baseJoeRewardsPerYear: ', baseJoeRewardsPerYear)
+        console.log('baseJoeAPR', baseJoeAPR)
+        return baseJoeAPR + basePoolAPR
+    }
 
     useEffect(() => {
         const totalInPool = selectedPair.reserve0 * selectedPair.reserve1
@@ -242,71 +282,88 @@ export default function Calc() {
     }
 
     return (
-        <div>
-            <h1>Booster Calculator</h1>
-            <h3>My Staked Deposit</h3>
-            <h3>Address</h3>
-            <input style={{width: "300px", marginBottom: "10px"}} value={userAddr} type='text' placeholder='account address' onChange={e => setUserAddr(e.target.value)}/>
-            <div style={{marginBottom: "10px"}}>
-                {loading ? 'Loading pairs...' : ''}
-            </div>
-            <select value={selectedPairID} onChange={(e) => setSelectedPairID(e.target.value)}>
-                {boostedPairOptions}
-            </select>
-            <h3>Pool liquidity</h3>
-            <div style={{marginBottom: "20px"}}>
-                <input type="number" placeholder={selectedPair?.totalSupply} />
-            </div>
-            <div>
-                <h4>{selectedPair?.token0?.symbol}</h4>
-                <input type="number" value={token0Value} onChange={(e) => setTokenAmount(1, e.target.value)} placeholder={selectedPair?.token0?.symbol} />
-            </div>
-            <div>
-                <h4>{selectedPair?.token1?.symbol}</h4>
-                <input type="number" value={token1Value} onChange={(e) => setTokenAmount(0, e.target.value)} placeholder={selectedPair?.token1?.symbol}/>
-            </div>
-            <div style={{display: "flex", justifyContent: "center", marginTop: "20px"}}>
-                <div style={{marginRight: "20px"}}>Pool Share</div>
-                <div>
-                    {
-                        poolShare > 0.1
-                        ?
-                        <div>{poolShare}%</div>
-                        :
-                        <div>&lt;0.1%</div>
-                    }
+        <div class='calc-container'>
+            <div class="calc-inner-container">
+                <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                    <img width="45" height="45" src={joe} style={{marginRight: "10px"}} />
+                    <h1 class='header'>Boosted Calculator</h1>
                 </div>
-            </div>
-
-            <select style={{marginTop: "15px"}} value={veJoeOrJoe} onChange={e => setVeJoeOrJoe(e.target.value)}>
-                <option value="veJoe">veJoe</option>
-                <option value="Joe">Joe</option>
-            </select>
-
-            <h3>My {veJoeOrJoe}</h3>
-            <input value={userVeJoe?.veJoeBalance ? userVeJoe.veJoeBalance : 0} type="number" onChange={(e) => handleJoeStake(e.target.value)}/>
-
-            <h3>Total veJoe Supply</h3>
-            <input type="number" value={veJoeSupply} onChange={(e) => setVEJoeSupply(e.target.value)} />
-
-            <div style={{display: "flex", justifyContent: "center"}}>
-                <div style={{marginRight: "20px"}}>veJoe share</div>
-                <div>
-                    {(joeStake/veJoeSupply).toFixed(2) > 0.1 ? (joeStake/veJoeSupply).toFixed(2) : '<0.1'}%
+                <h3>Address</h3>
+                <input style={{width: "300px", marginBottom: "10px"}} value={userAddr} type='text' placeholder='account address' onChange={e => setUserAddr(e.target.value)}/>
+                <h3>Pool</h3>
+                {
+                    loading ?
+                    (
+                    <div style={{marginBottom: "10px"}}>
+                        {loading ? 'Loading pools...' : ''}
+                    </div>
+                    )
+                    :
+                    (
+                    <select value={selectedPairID} onChange={(e) => setSelectedPairID(e.target.value)}>
+                        {boostedPairOptions}
+                    </select>
+                    )
+                }
+                <h3>Pool liquidity</h3>
+                <div style={{marginBottom: "20px"}}>
+                    <input type="number" placeholder={selectedPair?.totalSupply} />
                 </div>
-            </div> 
-            <div style={{display: "flex", justifyContent: "center"}}>
-                <div style={{marginRight: "20px"}}>Base APR</div>
-                <div>{baseAPR}%</div>
+                <div class='pool-container'>
+                    <div style={{marginRight: "10px"}}>
+                        <h4>{selectedPair?.token0?.symbol}</h4>
+                        <input type="number" value={token0Value} onChange={(e) => setTokenAmount(1, e.target.value)} placeholder={selectedPair?.token0?.symbol} />
+                    </div>
+                    <div>
+                        <h4>{selectedPair?.token1?.symbol}</h4>
+                        <input type="number" value={token1Value} onChange={(e) => setTokenAmount(0, e.target.value)} placeholder={selectedPair?.token1?.symbol}/>
+                    </div>
+                </div>
+                <div style={{display: "flex", justifyContent: "center", marginTop: "20px"}}>
+                    <div style={{marginRight: "20px"}}>Pool Share</div>
+                    <div>
+                        {
+                            poolShare > 0.1
+                            ?
+                            <div>{poolShare}%</div>
+                            :
+                            <div>&lt;0.1%</div>
+                        }
+                    </div>
+                </div>
+
+                <select style={{marginTop: "15px"}} value={veJoeOrJoe} onChange={e => setVeJoeOrJoe(e.target.value)}>
+                    <option value="veJoe">veJoe</option>
+                    <option value="Joe">Joe</option>
+                </select>
+
+                <h3>My {veJoeOrJoe}</h3>
+                <input value={joeStake} type="number" onChange={(e) => handleJoeStake(e.target.value)}/>
+
+                <h3>Total veJoe Supply</h3>
+                <input style={{minWidth: "220px"}} type="number" value={veJoeSupply} onChange={(e) => setVEJoeSupply(e.target.value)} />
+
+                    <div class="output-container">
+                        <div class='output'>
+                            <div style={{marginRight: "20px"}}>veJoe share</div>
+                            <div class='white'>
+                                {(joeStake/veJoeSupply).toFixed(2) > 0.1 ? (joeStake/veJoeSupply).toFixed(2) : '<0.1'}%
+                            </div>
+                        </div> 
+                        <div class='output'>
+                            <div style={{marginRight: "20px"}}>Base APR</div>
+                            <div class='white'>{baseAPR.toFixed(2)}%</div>
+                        </div>
+                        <div class='output'>
+                            <div style={{marginRight: "20px"}}>Current Boosted APR</div>
+                            <div class='white'>{currentBoostedAPR.toFixed(2)}%</div>
+                        </div>
+                        <div class='output'>
+                            <div style={{marginRight: "20px"}}>Estimated Boosted APR</div>
+                            <div class='white'>{estimatedBoostedAPR.toFixed(2)}%</div>
+                        </div>
+                    </div>
             </div>
-            <div style={{display: "flex", justifyContent: "center"}}>
-                <div style={{marginRight: "20px"}}>Current Boosted APR</div>
-                <div>2.00%</div>
-            </div>
-            <div style={{display: "flex", justifyContent: "center"}}>
-                <div style={{marginRight: "20px"}}>Estimated Boosted APR</div>
-                <div>1.00%</div>
-            </div>
-        </div>
+        </div>        
     )
 }
